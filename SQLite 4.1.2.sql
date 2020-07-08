e70fab6d8b26acf00a8fc3b62ab8c7765d8c3569
Alexandra Saha <66027074+AlexandraSaha@users.noreply.github.com>
2020-07-08 17:12:14 -0400

    Add files via upload

---
+++ SQLite 4.1.2.sql
@@ -0,0 +1,12 @@
+INSERT INTO students (first_name, last_name, pet_name, pet_type, pet_breed)
+VALUES ('Harry', 'Potter', 'Hedwig', 'Owl', 'Snowy'),
+('Ronald', 'Weasley', 'Scabbers', 'Rat', 'Common Garden'),
+		('Hermoine', 'Granger', 'Crookshanks', 'Cat', 'Red Persian'),
+		('Neville', 'Longbottom', 'Trevor', 'Toad', 'Smooth-Sided'),
+        ('Ginny', 'Weasley', 'Arnold', 'Pygmy Puff', 'Magical'),
+        ('Draco', 'Malfoy', 'Ulysses', 'Owl', 'Eagle'),
+        ('Lee', 'Jordan', 'Greg', 'Tarantula', 'Goliath Birdeater'),
+        ('Percy', 'Weasley', 'Hermes', 'Owl', 'Screech'),
+        ('Lavender', 'Brown', 'Blinky', 'Rabbit', 'Holland Lop'),
+        ('Alexandra', 'Quick', 'Artemis', 'Owl', 'Barn') 
+        ;
