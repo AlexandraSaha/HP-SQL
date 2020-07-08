8cc05bd4c3c2bbbe67a6ce75dfd5e58d0820a243
Alexandra Saha <66027074+AlexandraSaha@users.noreply.github.com>
2020-07-08 17:12:33 -0400

    Update SQLite 4.1.2.sql

--- SQLite 4.1.2.sql
+++ SQLite 4.1.2.sql
@@ -1,8 +1,8 @@
 INSERT INTO students (first_name, last_name, pet_name, pet_type, pet_breed)
 VALUES ('Harry', 'Potter', 'Hedwig', 'Owl', 'Snowy'),
-('Ronald', 'Weasley', 'Scabbers', 'Rat', 'Common Garden'),
-		('Hermoine', 'Granger', 'Crookshanks', 'Cat', 'Red Persian'),
-		('Neville', 'Longbottom', 'Trevor', 'Toad', 'Smooth-Sided'),
+	('Ronald', 'Weasley', 'Scabbers', 'Rat', 'Common Garden'),
+	('Hermoine', 'Granger', 'Crookshanks', 'Cat', 'Red Persian'),
+	('Neville', 'Longbottom', 'Trevor', 'Toad', 'Smooth-Sided'),
         ('Ginny', 'Weasley', 'Arnold', 'Pygmy Puff', 'Magical'),
         ('Draco', 'Malfoy', 'Ulysses', 'Owl', 'Eagle'),
         ('Lee', 'Jordan', 'Greg', 'Tarantula', 'Goliath Birdeater'),
