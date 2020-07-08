a74981edcf80dd4d18e893545e61e21d23cef9f2
Alexandra Saha <66027074+AlexandraSaha@users.noreply.github.com>
2020-07-08 17:30:42 -0400

    Update SQLite 4.1.2.sql

--- SQLite 4.1.2.sql
+++ SQLite 4.1.2.sql
@@ -1,12 +1,14 @@
-INSERT INTO students (first_name, last_name, pet_name, pet_type, pet_breed)
-VALUES ('Harry', 'Potter', 'Hedwig', 'Owl', 'Snowy'),
-	('Ronald', 'Weasley', 'Scabbers', 'Rat', 'Common Garden'),
-	('Hermoine', 'Granger', 'Crookshanks', 'Cat', 'Red Persian'),
-	('Neville', 'Longbottom', 'Trevor', 'Toad', 'Smooth-Sided'),
-        ('Ginny', 'Weasley', 'Arnold', 'Pygmy Puff', 'Magical'),
-        ('Draco', 'Malfoy', 'Ulysses', 'Owl', 'Eagle'),
-        ('Lee', 'Jordan', 'Greg', 'Tarantula', 'Goliath Birdeater'),
-        ('Percy', 'Weasley', 'Hermes', 'Owl', 'Screech'),
-        ('Lavender', 'Brown', 'Blinky', 'Rabbit', 'Holland Lop'),
-        ('Alexandra', 'Quick', 'Artemis', 'Owl', 'Barn') 
+INSERT INTO students (first_name, last_name, house, pet_id) 
+VALUES ('Harry', 'Potter', 'Gryffindor', 101),
+	('Ronald', 'Weasley', 'Gryffindor', 102),
+	('Hermoine', 'Granger', 'Gryffindor', 103),
+	('Neville', 'Longbottom', 'Gryffindor', 104),
+        ('Ginny', 'Weasley', 'Gryffindor', 105),
+        ('Draco', 'Malfoy', 'Slytherin', 106),
+        ('Lee', 'Jordan', 'Gryffindor', 107),
+        ('Percy', 'Weasley', 'Gryffindor', 108),
+        ('Lavender', 'Brown', 'Gryffindor', 109),
+        ('Alexandra', 'Quick', 'Hufflepuff', 110) 
         ;
+	
+
